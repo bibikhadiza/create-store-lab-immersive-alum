514436b7c017cbd75eabc15eebf7979e04657b13
Bibi Khadiza <bibi_khadiza_accenture@whirlpool.com>
2018-03-19 16:46:13 -0400

    completed lab

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+  constructor(){
+    super()
+    this.state = {
+      username: '',
+      hometown: ''
+    }
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handleHometownChange = this.handleHometownChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleUsernameChange(ev){
+    this.setState({
+      username: ev.target.value
+    })
+  }
+
+  handleSubmit(ev){
+    ev.preventDefault()
+    this.props.store.dispatch({type: 'ADD_USER', user: {username: this.state.username, hometown: this.state.hometown}})
+  }
+
+  handleHometownChange(ev){
+    this.setState({
+      hometown: ev.target.value
+    })
+  }
+
   render() {
     return(
-      <div>
-        user input
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <input name="username" onChange={this.handleUsernameChange} value={this.state.username}/>
+        <input name="hometown" onChange={this.handleHometownChange} value={this.state.hometown} type="text"/>
+        <button></button>
+      </form>
     );
   }
 };
--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,12 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state = {
+  users: []
+}, action){
+  switch (action.type) {
+    case "ADD_USER":
+      let newUserState = state;
+      newUserState.users.push({username: action.user.username, hometown: action.user.hometown})
+      return newUserState;
+      default:
+      return state;
+  }
 }
